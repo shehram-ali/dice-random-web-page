5fa090a8a569cba53c469ba4579c0e75cd7b96a3
shehram-ali <66680856+shehram-ali@users.noreply.github.com>
2024-08-02 11:02:41 +0500

    Update dicee.js

--- dicee.js
+++ dicee.js
@@ -1,9 +1,26 @@
 window.onload=function(){
-    function displayimg(imgid){
+    function displayimg(imgid,imgid2){
         var imglist=["images/dice1.png","images/dice2.png","images/dice3.png","images/dice4.png","images/dice5.png","images/dice6.png"];
         var num=Math.floor(Math.random(imglist)*7);
+        var num2=Math.floor(Math.random(imglist)*7);
+
         document.getElementById(imgid).src=imglist[num];
-    }
-    displayimg('img1');
-    displayimg('img2');
+        document.getElementById(imgid2).src=imglist[num2];
+        if(num<num2){
+            document.querySelector("h1").innerHTML="Player 2 Win";
+        }
+        else if (num>num2){
+            document.querySelector("h1").innerHTML="Player 1 Win";
+
+        }
+        else{
+            document.querySelector("h1").innerHTML="Match Tie";
+
+        }
+
+
+    } 
+
+    displayimg('img1','img2');
+    
 }
